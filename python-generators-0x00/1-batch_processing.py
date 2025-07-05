6e85d2916493fa6e573606a674d69b2ccb21c38e
BelalEbrahim <fareidfareid131@gmail.com>
2025-07-06 01:30:07 +0300

    Merge remote changes

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """
-Fetch users in batches and filter by age > 25.
+Fetch users in batches and filter by age > 25 using generators.
 """
 from seed import connect_to_prodev
 
 def stream_users_in_batches(batch_size):
+    """Yield successive batches of users from the database."""
     conn = connect_to_prodev()
     cursor = conn.cursor(dictionary=True)
     offset = 0
@@ -22,6 +23,7 @@ def stream_users_in_batches(batch_size):
     conn.close()
 
 def batch_processing(batch_size):
+    """Print users older than 25 by streaming in batches."""
     for batch in stream_users_in_batches(batch_size):
         for user in batch:
             if user['age'] > 25:
