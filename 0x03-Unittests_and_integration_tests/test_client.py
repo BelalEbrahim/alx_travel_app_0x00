4ce9211a9595eb9168c6d0dc0a986e409a85e42e
YourName <your@email.com>
2025-07-20 21:52:07 +0300

    Initial commit

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -60,8 +60,9 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(client.has_license(repo, license_key), expected)
 
 
-@parameterized_class(('org_payload', 'repos_payload', 'expected_repos',
-                     'apache2_repos'), [TEST_PAYLOAD])
+@parameterized_class(('org_payload', 'repos_payload', 
+                     'expected_repos', 'apache2_repos'), 
+                     [TEST_PAYLOAD])
 class TestIntegrationGithubOrgClient(unittest.TestCase):
     """
     Integration tests for GithubOrgClient class
